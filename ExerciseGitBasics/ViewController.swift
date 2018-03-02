487ee308f29f47a4d6aeb29a3a945e5772c20f3c
Shruti Sharma <shruti25sharma@gmail.com>
2018-03-02 10:35:40 -0800

    Initial Commit

---
+++ ExerciseGitBasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExerciseGitBasics
+//
+//  Created by Shruti Sharma on 3/2/18.
+//  Copyright © 2018 Shruti Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
